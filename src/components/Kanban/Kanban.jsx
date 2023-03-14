b6b5dd40a1bbd75dfb1dd672f2ea5b8be80a16e2
Nazar Demchan <name.surname@plus4u.net>
2023-03-14 14:06:37 +0200

    some test kanban

---
+++ src/components/Kanban/Kanban.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Kanban(props) {
+  return <div>{'kanban'}</div>;
+}
+
+export default Kanban;
---
+++ src/pages/Kanban.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from '../Layouts/MainLayout/MainLayout';
+import Kanban from '../components/Kanban/Kanban';
+
+function Notations() {
+  return (
+    <MainLayout>
+      <Kanban />
+    </MainLayout>
+  );
+}
+
+export default Notations;
